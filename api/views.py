8a7544605d5bb19fee5d796fda0e98cc26b5aa94
Jared Haight <jhaight@redventures.com>
2014-12-06 21:50:40 -0500

    Making twilio do stuff

--- api/views.py
+++ api/views.py
@@ -1,10 +1,25 @@
 from django.http import HttpResponse
+
 from django_twilio.decorators import twilio_view
 from twilio.twiml import Response
+
 from dashboard.models import Result
 
 @twilio_view
 def sms(request):
     resp = Response()
-    resp.message("SCALE OF ONE TO A BILLION!")
+    from_number = None
+    body = None
+    if request.POST.get("from"):
+        from_number = request.POST.get('from')
+    if request.POST.get("body"):
+        body = request.POST.get("body")
+    if from_number and body:
+        resp.message("You said %s from %s" % (from_number, body))
+    elif from_number:
+        resp.message("You send a message from %s" % from_number)
+    elif body:
+        resp.message("You said %s" % body)
+    else:
+        resp.message("I have no idea what's going on.")
     return HttpResponse(resp.toxml(), content_type="text/xml")
