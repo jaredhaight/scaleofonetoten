bca51ff5f2764ab251b12316adf0ac00eddd99e8
Jared Haight <jhaight@redventures.com>
2014-12-06 22:29:50 -0500

    Making twilio do more stuff

--- api/views.py
+++ api/views.py
@@ -9,25 +9,24 @@ from profile.models import HayUser
 @twilio_view
 def sms(request):
     resp = Response()
-    try:
-        from_number = request.POST["From"]
-    except:
-        from_number = None
-    try:
-        body = request.POST["Body"]
-    except:
-        body = None
+    from_number = request.POST["From"]
+    body = request.POST["Body"]
     try:
         user = HayUser.objects.filter(from_number[1:]).first()
     except:
         user = None
+
+    try:
+        body = int(body)
+    except:
+        body = "Sorry, I only understand numbers."
 
     if type(body) == int and user:
         result = Result(value=body, user=user, source=from_number)
         result.save()
         resp.message("Got response of %s for %s" % (body, user))
     elif user:
-        resp.message("I'm sorry, I only understand numbers.")
+        resp.message(body)
     else:
         resp.message("Please sign up for an account at http://www.scaleofonetoten.com")
     return HttpResponse(resp.toxml(), content_type="text/xml")
