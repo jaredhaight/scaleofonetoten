94ce47b260f042895caff2871db3f8930fc337fd
Jared Haight <jhaight@redventures.com>
2014-12-26 17:55:49 -0500

    added user var to all views

--- onetoten/settings.py
+++ onetoten/settings.py
@@ -52,6 +52,17 @@ MIDDLEWARE_CLASSES = (
     'onetoten.middleware.TimezoneMiddleware'
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "onetoten.context_preprocessors.user_processor"
+)
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
