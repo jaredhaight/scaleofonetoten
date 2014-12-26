037f54ce93399481e3c1161bf11f2dd89ee72fa7
Jared Haight <jhaight@redventures.com>
2014-12-26 17:05:47 -0500

    fixed login redirection

--- onetoten/views.py
+++ onetoten/views.py
@@ -13,13 +13,30 @@ def register(request):
 def login_view(request):
     logout(request)
     username = password = ''
+    try:
+        next_value = request.GET["next"]
+    except:
+        next_value = None
+
     if request.POST:
         username = request.POST['username']
         password = request.POST['password']
+        try:
+            next_value = request.POST['next_value']
+        except:
+            next_value = None
 
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
                 login(request, user)
+                if next_value:
+                    return HttpResponseRedirect(next_value)
                 return HttpResponseRedirect('/profile/')
-    return render_to_response('login.html', context_instance=RequestContext(request))
+    d = dict(next_value=next_value)
+    return render_to_response('login.html', d, context_instance=RequestContext(request))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/")
