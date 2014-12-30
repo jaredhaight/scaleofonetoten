9e100b786757c9ceef9f5c72169c64842714d35f
Jared Haight <jhaight@redventures.com>
2014-12-29 20:50:04 -0500

    Updating

--- onetoten/utils.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import TwilioRestClient
-from onetoten.local_settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
-client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-from_number = "19802014736"
-
-
-def send_text(to_number, message_content):
-    result = client.messages.create(to=to_number, from_=from_number, body=message_content)
-    return result
