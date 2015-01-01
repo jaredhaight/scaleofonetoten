f468b60d1825176603253add61a2ed3595718401
Jared Haight <jhaight@redventures.com>
2014-12-31 22:20:23 -0500

    Adjusting logging

--- onetoten/utils.py
+++ onetoten/utils.py
@@ -1,5 +1,7 @@
 from twilio.rest import TwilioRestClient
 from onetoten.local_settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+import datetime
+
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 from_number = "19802014736"
 
